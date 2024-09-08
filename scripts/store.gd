7e444c4e5551d111243a0bc1e26c4daea4ae6c93
BlazingNeutron <steven.armstrong+github@gmail.com>
2024-09-08 18:43:23 -0400

    Store menu always visible on right

--- scripts/store.gd
+++ scripts/store.gd
@@ -11,8 +11,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_close_pressed() -> void:
-	print("hiding")
-	emit_signal("hide")
