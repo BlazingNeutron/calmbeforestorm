10206b8a8825b2552c59eb099f259178c9bfb1d3
BlazingNeutron <steven.armstrong+github@gmail.com>
2024-09-08 15:13:10 -0400

    initial proejct setup and some art

---
+++ scripts/waste.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var SPEED = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y += delta * SPEED
+	
