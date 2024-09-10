2018b3107bdc8196a62930249b007107b29723d6
Colin <colin.armstrong8@gmail.com>
2024-09-09 17:07:53 -0700

    Main menu initial
    - Added Main menu scene
    - Created Menu Theme
    - Set default load scene to main menu

---
+++ scripts/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/Start.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
